e088900fa0f54070f556a5a9dc854d5a74fac514
Vũ Hoàng Anh <142197061+vamcrizer@users.noreply.github.com>
2023-10-13 00:46:19 +0700

    Update EC06009.c

--- EC06009.c
+++ EC06009.c
@@ -2,6 +2,18 @@
 #include <string.h>
 #include <ctype.h>
 
+char *strlwr(char *str)
+{
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p) {
+     *p = tolower((unsigned char)*p);
+      p++;
+  }
+
+  return str;
+}
+
 int countWords(char* str) {
 
   int count = 0;
